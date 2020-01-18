aa3bd5bee3fe3e8e000f78fbe02c8b9ce1e4055a
tigiridon <boss@ccsps.ru>
2020-01-18 22:45:09 +0300

    hw

--- DB/AGR_TEST/HW Script-7.sql
+++ DB/AGR_TEST/HW Script-7.sql
@@ -1,7 +1,27 @@
-SELECT  * FROM storehouses_products  WHERE value > 0 ORDER BY value
-UNION 
-SELECT  * FROM storehouses_products   WHERE value < 1 ORDER BY  value;
+
+UPDATE users 
+SET
+created_at = now(),
+updated_at =now();
 
 
+UPDATE users 
+SET
+created_at = STR_TO_DATE(created_at, '%d.%m.%Y %k:%i'),
+updated_at =(created_at, '%d.%m.%Y %k:%i');
+
+
+SELECT name FROM users WHERE DATE_FORMAT(birthday_at, '%M') IN ('may', 'august')
+
+SELECT  * FROM storehouses_products ORDER BY IF(value > 0, 0,1)  value;
+
+SELECT * FROM catalogs WHERE id IN (5, 1, 2)
+ORDER BY FIELD(id, 5, 1, 2)
+
+
+SELECT AVG(TIMESTAMPDIFF(YEAR, birthday_at, NOW())) AS age FROM users;
 
 SELECT AVG(value) FROM users;
+
+
+SELECT ROUND(EXP(SUM(LN(id)))) FROM catalogs;
