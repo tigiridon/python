1f5aab179ff47d6f886f5933345aebef0d5d807e
tigiridon <boss@ccsps.ru>
2020-02-07 00:39:45 +0300

    hw11

--- DB/hw/hw11.sql
+++ DB/hw/hw11.sql
@@ -2,56 +2,62 @@
 
 DROP TABLE IF EXISTS logs;
 CREATE TABLE logs (
-  id SERIAL PRIMARY KEY,
-  name_table VARCHAR(255),
- name_cat VARCHAR(255),
-id_key INT UNSIGNED,
-created_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP 
+  id int unsigned not null auto_increment PRIMARY KEY,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP ,
+  name_table VARCHAR(255) not null,
+  id_key INT unsigned not null,
+ name_cat VARCHAR(255) not null
 ) 
 ENGINE = Archive;
 
 
 DELIMITER //
 
-CREATE TRIGGER logs_products_insert BEFORE INSERT ON products
+CREATE TRIGGER logs_products_insert after INSERT ON products
 FOR EACH ROW BEGIN
-  IF NEW.id  then
-  INSERT INTO logs (name_table, name_cat, id_key, created_at ) 
- values (
- 'products' ,(select name from products  where products.id = max(products.id)), 
-(select id from products where products.id = max(products.id)), 
-now()) 
-) 
-  END IF;
+  INSERT INTO logs values (null, default, 'products', NEW.id, NEW.name );
 END//
 
-CREATE TRIGGER logs_catalogs_insert BEFORE INSERT ON catalogs
+CREATE TRIGGER logs_catalogs_insert after INSERT ON catalogs
 FOR EACH ROW BEGIN
-  IF NEW.id  then
-  INSERT INTO logs (name_table, name_cat, id_key, created_at ) 
- values (
- 'catalogs' ,(select name from catalogs  where catalogs.id = max(catalogs.id)), 
-(select id from catalogs  where catalogs.id = max(catalogs.id)), 
-now()) 
-) 
-  END IF;
+  INSERT INTO logs values (null, default, 'products', NEW.id, NEW.name );
+END//
+
+CREATE TRIGGER logs_users_insert after INSERT ON users
+FOR EACH ROW BEGIN
+  INSERT INTO logs values (null, default, 'products', NEW.id, NEW.name );
 END//
 
 DELIMITER ;
- 
+
 
 redis-cli
 
-SADD count_ip ip1212.31.23123.1231 ip12123.123123.1231 ip12123.123123.1231 ip121323.1263123.1231
-SCARD count_ip
-SADD email igorsimdyanov@gmail.com igor@simdyanov.ru igor@softtime.ru i.simdyanov@rambler-co.ru
-SADD email
-HMSET admin login "root" email "igor@simdyanov.ru" 
-HGET admin login
-HVALS admin
-HKEYS admin
-HGETALL admin
 
-mongo
+HSET emails  'vasa_pupakin' 'pupakin@gmail.com' 'doramkin_pasha' 'doramkin_pasha@softtime.ru'
+
+HGET emails 'doramkin_pasha'
+HSET polzovateli 'pupakin@gmail.com' 'vasa_pupakin' 'doramkin_pasha@softtime.ru' 'doramkin_pasha'
+HGET  polzovateli 'pupakin@gmail.com'
+
+
+
+
+show dbs
+use shop
+
+db.createcollection('catalogs')
+db.createcollection('products')
+db.catalogs.insert({name: 'Процессоры'})
+db.catalogs.insert({name: 'Видеокарты'})
+db.catalogs.insert({name: 'Мат.платы'})
+db.products.insert({name: 'Intel core i66-7777', desription: 'Процессор ПК, на базе селерон', price: 6666.77, catalog_id: new ObjectID("asda2112asc121asa3777")});
+db.products.insert({name: 'Intel core i22-788', desription: 'Процессор ПК, на базе пенька', price: 3232.17, catalog_id: new ObjectID("asda2112asc121asa3777")});
+db.products.find()
+db.products.find({catalog_id: new ObjectID("asda2112asc121asa3777")})
+
+
+
+
 
  
